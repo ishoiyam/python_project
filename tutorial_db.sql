9a464706567474dcf469f3d3f50a33b7a761b7bb
ishoiyam <ishoiyam@protonmail.com>
2022-02-04 12:11:00 +0100

    last changes

---
+++ tutorial_db.sql
@@ -0,0 +1,24 @@
+
+drop database if exists `karim_benzema_db` ; 
+create database `karim_benzema_db`;
+-- you have to select the db that you want to work with
+USE `karim_benzema_db`;
+
+create table student (
+	student_id int(11) auto_increment not null,
+	first_name varchar(50) not null,
+	last_name varchar(50) not null,
+	birth_date date not null,
+	
+	primary key (student_id)
+
+) ENGINE=InnoDB DEFAULT charset=utf8 collate=utf8_general_ci;
+
+insert into student (first_name, last_name, birth_date) values ("ahmed", "icho", "2000-01-01");
+
+select * from student;
+select first_name, birth_date, last_name from student;
+
+-- this is just a way to write/have a comment in mysql file (.sql)
+-- comments are being ignored by mysql
+-- ENGINE=InnoDB DEFAULT charset=utf8 collate=utf8_general_ci;
